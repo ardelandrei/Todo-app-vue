557866899084c51faac283b3a68ee3f628600373
Andrei Ardel <A.Ardel@netmatch.ro>
2018-07-16 15:20:59 +0300

    simple todo app

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">    
+    <todo-list v-bind:todos="todos"></todo-list>
+    <create-todo v-on:add-todo="addTodo"></create-todo>
+  </div>
+</template>
+
+<script>
+import TodoList from "./components/TodoList";
+import CreateTodo from "./components/CreateTodo";
+
+export default {
+  name: "App",
+  components: {
+    TodoList,
+    CreateTodo,
+  },
+
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo A",
+          project: "Project A",
+          done: false
+        },
+        {
+          id: 2,
+          title: "Todo B",
+          project: "Project B",
+          done: true
+        },
+        {
+          id: 3,
+          title: "Todo C",
+          project: "Project C",
+          done: false
+        },
+        {
+          id: 4,
+          title: "Todo D",
+          project: "Project D",
+          done: false
+        }
+      ]
+    };
+  },
+
+  methods: {
+    addTodo(todo) {
+
+      const title = todo.title;
+      const project = todo.project;
+
+      this.todos.push({
+        title,
+        project,
+        done: false
+      });
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
---
+++ src/components/TodoList.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <p>Completed tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+        <p>Pending tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+
+        <todo 
+            v-on:delete-todo="deleteTodo" 
+            v-on:complete-todo="completeTodo"  
+            v-for="todo in todos" 
+            v-bind:todo="todo"
+            v-bind:key="todo.id">
+        </todo>
+    </div>
+</template>
+
+<script>
+
+import Todo from './Todo';
+
+export default {
+    props: ['todos'],
+
+    components: {
+        Todo,
+    },
+
+    methods: {
+        deleteTodo(todo) {
+
+            const todoIndex = this.todos.indexOf(todo);            
+            this.todos.splice(todoIndex, 1);
+        },
+
+        completeTodo(todo) {
+            const todoIndex = this.todos.indexOf(todo);
+            this.todos[todoIndex].done = true;
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
